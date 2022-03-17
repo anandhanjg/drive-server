0499ec9acc7c53aa45fd6958bcff2f72b2255109
anandhanjg <jganandhanddevlabs@gmail.com>
2022-03-17 08:19:51 +0530

    https added

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const { setMiddlewares } = require('./middleware');
 const { setRoutes } = require('./routes');
 const mongoConnect=require('./config/mongoose.config');
 const app=express();
-
+const https=require('https');
+let myDomain="anandhan.ddl.link"
 setMiddlewares(app,express);
 setRoutes(app,express);
-
+const server=https.createServer({
+    key:fs.readFileSync(`/etc/letsencrypt/live/${myDomain}/privkey.pem`),
+    cert:fs.readFileSync(`/etc/letsencrypt/live/${myDomain}/fullchain.pem`)
+},app)
 mongoConnect.connection.on('connected',async ()=>{
-    app.listen(4000,()=>{
+    server.listen(4000,()=>{
         console.log("SERVER IS RUNNING ON PORT 4000")
     })
 });
